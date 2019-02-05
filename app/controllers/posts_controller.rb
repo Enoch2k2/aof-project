6f4b5035c5bbb50540b1638e29448dd8ed601099
Enoch2k2 <enoch2k2@hotmail.com>
2019-02-05 12:36:40 -0600

    gave ability to add image link for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:title, :content, :topic_id, :user_id)
+      params.require(:post).permit(:title, :content, :topic_id, :user_id, :image_link)
     end
 
     def authorize_user!
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_044329) do
+ActiveRecord::Schema.define(version: 2019_02_05_182919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2019_02_05_044329) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_link"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
